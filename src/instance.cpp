7fd41d1ec3dc1395b040df2b1ff873570374d7a8
stplaydog <stplaydog@leon.c.smart-momentum-98723.internal>
2015-08-01 14:39:34 +0000

    some changes in google cloud

--- src/instance.cpp
+++ src/instance.cpp
@@ -29,50 +29,38 @@ Instance::Instance(const char* lf)
     }
 }
 
-Instance::~Instance(){
+/**
+ * Factory pattern implementation.
+**/
+Instance *make_instance(int choice, const char *f, const char *od, const char *lf)
+{
+    if(choice == OPTKIT_INS_MC)
+    {
+        return new InsMC(f, od, lf);
+    }
+    else if(choice == OPTKIT_INS_DIS)
+    {
+        return new InsDis(f, is_exe, lf, const char* fd, const char* fbi, const bool *is_cal_bij) : 
+    }
+    else if(choice == OPTKIT_INS_KNAP)
+    {
+    }
+    else if(choice == OPTKIT_INS_MED)
+    {
+    }
 }
 
-
-void 
-Instance::to_branch(int which_branch){
-}
-
-void 
-Instance::from_branch(){
-}
-
-void 
-Instance::compute_bound(){
-}
-
-int  
-Instance::get_num_branches(){
-    return 0;
-}
-
-int  
-Instance::get_encode(int *encode){
-    return 0;
-}
-
-void  
-Instance::to_encode(int *encode, int size){
-}
-
-void
-Instance::print_encode(){
-}
-
-void 
-Instance::copy_code(int *encode, int size){
-}
-
-int
-Instance::get_value(){
-    return 0;
-}
-
-int 
-Instance::return_private_info(){
-    return 0;
-}
+/**
+ * Virtual functions.
+**/
+Instance::~Instance(){}
+void Instance::to_branch(int which_branch){}
+void Instance::from_branch(){}
+void Instance::compute_bound(){}
+int  Instance::get_num_branches(){return 0;}
+int  Instance::get_encode(int *encode){return 0;}
+void Instance::to_encode(int *encode, int size){}
+void Instance::print_encode(){}
+void Instance::copy_code(int *encode, int size){}
+int  Instance::get_value(){return 0;}
+int  Instance::return_private_info(){return 0;}
