3533322b7c4132d1454cace32e8a3f938faabae1
zhaoming.yin@oracle.com <zyin@slc08usu.us.oracle.com>
2015-12-14 20:38:22 -0800

    add gt_truss

---
+++ test/gt_truss.cpp
@@ -0,0 +1,44 @@
+/**
+ * Copy right YMSys, 2015, Zhaoming Yin
+ *
+ *  @brief    Unittest of truss decomposition utilities 
+ *
+ *  MODIFIED   (MM/DD/YY)
+ *  stplaydog   12/14/15 - Creation
+**/
+
+#include <gtest/gtest.h>
+#include "truss.h"
+#include "test_util.h" 
+
+/**
+ * @brief   Test a small graph one color
+ *
+**/
+TEST(InitTrussTest_1, Success)
+{
+}
+
+/**
+ * @brief   test compute_sup  
+ *
+**/
+TEST(ComputeSupTest_1, Success)
+{
+}
+
+/**
+ * @brief   test sup_e_opr 
+ *
+**/
+TEST(SupEOprTest_1, Success)
+{
+}
+
+/**
+ * @brief   test reduce_one_edge  
+ *
+**/
+TEST(TrussDecompositionTest_1, Success)
+{
+}
