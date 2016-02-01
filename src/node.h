711e89322c9d7dbf534543131a47230e45cd4c15
zhaoming.yin@oracle.com <zyin@slc08usu.us.oracle.com>
2016-01-31 21:37:35 -0800

    design the list class

--- src/node.h
+++ src/node.h
@@ -4,6 +4,7 @@
  *  @brief    Basic list implementation.
  *
  *  MODIFIED   (MM/DD/YY)
+ *  stplaydog   01/31/16 - defined the interface. 
  *  stplaydog   01/30/16 - Creation
 **/
 
@@ -16,14 +17,21 @@
 
 using namespace std;
 
+/**
+ * @interface   for a representation of a search node that can be 
+ *              serialized or deserialized.
+ *
+**/
+template <class T>
 class Node 
 {
 public:
     Node() {};
     ~Node() {};
 
-    virtual void serialize() = 0;
-    virtual void deserialize() = 0;
+    virtual void serialize(T* buf, int32_t &idx) = 0;
+    virtual void deserialize(T* buf, int32_t &idx) = 0;
+    virtual void get_encode_size() = 0;
 };
 
 #endif
