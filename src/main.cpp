21f723a212d175c8fcf094578b225fb1b56da1f8
Zhaoming Yin <stplaydog@Zhaomings-Mac-mini.local>
2016-03-05 19:05:44 -0800

    remove one redundant file

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
  * @brief    This is the main entry of OPTKIT. 
  *
  *  MODIFIED   (MM/DD/YY)
+ *  stplaydog   03/05/16 - Use the new parser 
  *  stplaydog   12/13/15 - Fixed bugs related to print_help() 
  *  stplaydog   08/02/15 - Fixed bugs to make it compile.
  *  stplaydog   08/01/15 - Creation
@@ -22,10 +23,11 @@
 void define_arguments(Parser &parser);
 void process(Parser &parser);
 
-BoolOption    truss         ('t', "truss",      false, "perform the truss decomposition");
-BoolOption    stmodel       ('s', "stmodel",    false, "perform stmodel computation");
-StringOption  input         ('i', "input",      true , "input file name");
-StringOption  output        ('o', "output",     true , "output file name");
+BoolOption        truss         ('t', "truss",      false, "perform the truss decomposition");
+BoolOption        stmodel       ('s', "stmodel",    false, "perform stmodel computation");
+StringOption      input         ('i', "input",      true , "input file name");
+StringOption      output        ('o', "output",     true , "output file name");
+StringListOption  coord         ('c', "coord",      false, "spatial temporal coordinates");
 
 /**
  * Main function of OPTKIT
@@ -42,14 +44,25 @@ int32_t main(int32_t argc , const char *argv[])
     return 1;
 }
 
+/**
+ * @brief       This function is used to load parameters
+ *
+ * @param[in/out]       parser      This is the parameter parser
+**/
 void define_arguments(Parser &parser)
 {
     parser.addOption(truss)
           .addOption(stmodel)
           .addOption(input)
-          .addOption(output);
+          .addOption(output)
+          .addOption(coord);
 }
 
+/**
+ * @brief       process input parameters and perform according functions
+ *
+ * @param[in/out]       parser      This is the parameter parser
+**/
 void process(Parser &parser)
 {
     string infile = input.getValue();
@@ -63,10 +76,27 @@ void process(Parser &parser)
     }
     else if(stmodel.isSet())
     {
-        CrimeSTModel stm(infile.c_str());
-        vector<pair<int32_t, int32_t>> edges = stm.build_edges(1, 1, 1);
-        CSR g(edges);
-        Truss t(g.get_num_e(), g.get_num_c());
-        t.truss_decomosition(g, oufile.c_str(), 5);
+        if(coord.isSet())
+        {
+            list<string> values = coord.getValue();
+            int     _coord[3];
+            uint8_t i = 0;
+
+            for(auto entry = values.begin(); entry != values.end(); ++entry) 
+            {
+                _coord[i++] = stoi(*entry);
+            }
+
+            CrimeSTModel stm(infile.c_str());
+            vector<pair<int32_t, int32_t>> edges = stm.build_edges(_coord[0], _coord[1], _coord[2]);
+            CSR g(edges);
+            Truss t(g.get_num_e(), g.get_num_c());
+            t.truss_decomosition(g, oufile.c_str(), 5);
+        }
+        else
+        {
+            fprintf(stderr, "You should specify coordiates!\n");
+            ERROR_PRINT();
+        }
     }
 }
