9b5617dab4f02fc5874e089a1d4d408c8e881e89
Zhaoming Yin <stplaydog@Zhaomings-Mac-mini.local>
2016-03-10 22:55:58 -0800

    sth

---
+++ src/Stats.cpp
@@ -0,0 +1,14 @@
+/**
+ * Copy right YMSys, 2015,2016 Zhaoming Yin
+ *
+ * @brief    This is the class for keeping the program running stats
+ *           The result could be serialized to a json file.  
+ *
+ *  MODIFIED   (MM/DD/YY)
+ *  stplaydog   03/10/16 - Creation
+ *
+**/
+
+#include "Stats.h"
+
+Stats *Stats::m_instance = 0;
