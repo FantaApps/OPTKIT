ddc110e9feca08d072945e71f69edbada2922fa8
Zhaoming Yin <stplaydog@Zhaomings-Mac-mini.local>
2016-03-15 21:46:23 -0700

    add some boost algorithms

--- src/bgl.h
+++ src/bgl.h
@@ -4,6 +4,8 @@
  * @brief    This is the class for BGL using boost graph library. 
  *
  *  MODIFIED   (MM/DD/YY)
+ *  stplaydog   03/15/16 - Add clustering coefficient, betweeness centrality,
+ *                         and all cliques algorithms. 
  *  stplaydog   03/14/16 - Add all pairs shortest path 
  *  stplaydog   03/13/16 - Creation
  *
@@ -17,6 +19,7 @@
 #include <algorithm>
 #include <map>
 #include "gtest/gtest_prod.h"
+#include "Stats.h"
 
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/floyd_warshall_shortest.hpp>
@@ -24,14 +27,19 @@
 #include <boost/graph/exterior_property.hpp>
 #include <boost/graph/clustering_coefficient.hpp>
 
+#include <boost/graph/undirected_graph.hpp>
+#include <boost/graph/exterior_property.hpp>
+#include <boost/graph/clustering_coefficient.hpp>
+#include "../libs/helper.hpp"
+
+#include <boost/graph/bc_clustering.hpp>
+#include <boost/graph/iteration_macros.hpp>
+
+#include <boost/graph/bron_kerbosch_all_cliques.hpp>
+
 using namespace boost;
 using namespace std; 
 
-// The Actor type stores the name of each vertex in the graph.
-struct Actor
-{
-    string name;
-};
 
 typedef double                                      t_weight;
 typedef property<edge_weight_t, double>             EdgeWeightProperty;
@@ -42,15 +50,34 @@ typedef property_map<Adj, edge_weight_t>::type      WeightMap;
 typedef exterior_vertex_property<Adj, t_weight>     DistanceProperty;
 typedef DistanceProperty::matrix_type               DistanceMatrix;
 typedef DistanceProperty::matrix_map_type           DistanceMatrixMap;
-//typedef undirected_graph<Actor>                     Udir;
-//typedef graph_traits<Udir>::vertex_descriptor       UVertex;
-//typedef graph_traits<Udir>::edge_descriptor         Edge;
-//typedef property_map<Udir, string Actor::*>::type   NameMap;
-//typedef unsigned long                               t_indx;
-//typedef exterior_vertex_property<Udir, float>       ClusteringProperty;
-//typedef ClusteringProperty::container_type          ClusteringContainer;
-//typedef ClusteringProperty::map_type                ClusteringMap;
 
+
+struct Actor
+{
+    string name;
+};
+typedef undirected_graph<Actor>                     Udir;
+typedef graph_traits<Udir>::vertex_descriptor       UVertex;
+typedef graph_traits<Udir>::edge_descriptor         Edge;
+typedef property_map<Udir, string Actor::*>::type   NameMap;
+typedef unsigned long                               t_indx;
+typedef exterior_vertex_property<Udir, float>       ClusteringProperty;
+typedef ClusteringProperty::container_type          ClusteringContainer;
+typedef ClusteringProperty::map_type                ClusteringMap;
+
+struct EdgeProperties
+{
+      int weight;
+};
+typedef adjacency_list< setS, vecS, 
+                        undirectedS, no_property, 
+                        EdgeProperties >            Adj1;
+typedef Adj1::vertex_descriptor                     Vertex1;
+typedef Adj1::edge_descriptor                       Edge1;
+typedef std::map<Edge1, int>                        StdEdgeIndexMap;
+typedef associative_property_map< StdEdgeIndexMap > EdgeIndexMap;
+typedef property_map< Adj1, vertex_index_t>::type   VertexIndexMap;
+ 
 
 /**
  * @class BGL 
@@ -60,27 +87,71 @@ typedef DistanceProperty::matrix_map_type           DistanceMatrixMap;
 **/
 class BGL
 {
+    template <typename OutputStream>
+    struct clique_printer
+    {
+        clique_printer(OutputStream& stream)
+            : os(stream)
+        { }
+
+        template <typename Clique, typename Graph>
+            void clique(const Clique& c, const Graph& g)
+            {
+                typename Clique::const_iterator i, end = c.end();
+                for(i = c.begin(); i != end; ++i) {
+                    os << g[*i].name << ",";
+                }
+                os << endl;
+            }
+        OutputStream& os;
+    };
 
     public:
 
-        BGL(const char *in_file);
+        BGL(const char *in_file)
+        {
+        }
 
         BGL(vector<pair<int32_t, int32_t>> &edges)
         {
+            NameMap nm(get(&Actor::name, m_udir));
+            std::map<std::string, UVertex> verts;
+
             for(auto it = edges.begin(); it != edges.end(); ++it)
             {
                 add_edge(it->first, it->second, 1.0, m_adj);
-                //add_edge(it->first, it->second, 1, m_udir);
+
+                string name1 = to_string(it->first);
+                string name2 = to_string(it->second);
+                if(verts.find(name1) == verts.end())
+                {
+                    add_named_vertex(m_udir, nm, name1, verts);
+                }
+                if(verts.find(name2) == verts.end())
+                {
+                    add_named_vertex(m_udir, nm, name2, verts);
+                }
+
+                add_edge(verts[name1], verts[name2], m_udir);
+
+                add_edge(it->first, it->second, m_adj1);
+
             }
         }
 
+        /**
+         * @brief       Constructor and Destructor
+         *
+        **/
         BGL() {};
-
-
         ~BGL() {};
 
     private:
 
+        /**
+         *
+         *
+        **/
         void floyd_warshall()
         {
             WeightMap weight_pmap = boost::get(boost::edge_weight, m_adj);
@@ -94,24 +165,63 @@ class BGL
             {
                 ERROR_PRINT();
             }
-
         }
 
-        //void clustering_coeff()
-        //{
-        //    // Create the graph and a name map that provides access to
-        //    // then actor names.
-        //    NameMap nm(get(&Actor::name, m_udir));
+        /**
+         *
+         *
+        **/
+        void clustering_coeff()
+        {
+            ClusteringContainer coefs(num_vertices(m_udir));
+            ClusteringMap cm(coefs, m_udir);
+            float cc = all_clustering_coefficients(m_udir, cm);
+           
+            string val = to_string(cc); 
+            Stats::instance()->write_content(Stats::CLUSTERCOEFF, val);
+        }
 
-        //    ClusteringContainer coefs(num_vertices(m_udir));
-        //    ClusteringMap cm(coefs, m_udir);
-        //    float cc = all_clustering_coefficients(m_udir, cm);
-        //    Stats::instance->set(CC, cc);
-        //}
+        /**
+         *
+         *
+        **/
+        void betweeness_centrality()
+        {
+            StdEdgeIndexMap              my_e_index;
+            EdgeIndexMap                 e_index(my_e_index);
 
-        //std::map<Vertex, std::map<Vertex, int> >     matrix;
-        Adj                                          m_adj;
-        //Udir                                         m_udir;
+            int i = 0;
+            BGL_FORALL_EDGES(edge, m_adj1, Adj1)
+            {
+                my_e_index.insert(pair< Edge1, int >( edge, i));
+                ++i;
+            }
+
+            vector< double > e_centrality_vec(num_edges(m_adj1), 0.0);
+            iterator_property_map <vector<double>::iterator, EdgeIndexMap>
+                                  e_centrality_map(e_centrality_vec.begin(), e_index);
+
+            VertexIndexMap        v_index = get(vertex_index, m_adj1);
+            vector<double>        v_centrality_vec(num_vertices(m_adj1), 0.0);
+            iterator_property_map <vector< double >::iterator, VertexIndexMap >
+                                  v_centrality_map(v_centrality_vec.begin(), v_index);
+
+            brandes_betweenness_centrality( m_adj1, v_centrality_map, e_centrality_map );
+        }
+
+        /**
+         *
+         *
+        **/
+        void all_cliques()
+        {
+            clique_printer<ostream> vis(cout);
+            bron_kerbosch_all_cliques(m_udir, vis);
+        }
+
+        Adj  m_adj;     ///<
+        Adj1 m_adj1;    ///<
+        Udir m_udir;    ///<
 
         FRIEND_TEST(InitGraphTest_2, Success);
 };
