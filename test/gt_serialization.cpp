1fd780f79c1afa515a754df71423726279c9b793
Cloud User <ec2-user@ip-172-31-5-200.us-west-2.compute.internal>
2016-07-10 18:44:11 -0400

    finish all the serialization test

--- test/gt_serialization.cpp
+++ test/gt_serialization.cpp
@@ -4,7 +4,7 @@
  *  @brief    Unittest of CSR graph structure. 
  *
  *  MODIFIED   (MM/DD/YY)
- *  stplaydog   07/10/16 - Creation
+ *  stplaydog   07/10/16 - Finish all the tests 
  *  stplaydog   07/03/16 - Creation
 **/
 
@@ -12,6 +12,7 @@
 #include <../src/Stats.h>
 #include <gtest/gtest.h>
 #include <fstream>
+#include "CrimeSTModel.h" 
 
 using namespace std;
 using boost::archive::text_oarchive;
@@ -19,7 +20,7 @@ using boost::archive::text_iarchive;
 using boost::archive::archive_exception;
 
 /**
- * @brief   Test a small graph one color
+ * @brief   Test serialize config 
  *
 **/
 TEST(ConfigSerializationTest_1, Success)
@@ -57,7 +58,7 @@ TEST(ConfigSerializationTest_1, Success)
 }
 
 /**
- * @brief   Test a small graph one color
+ * @brief   Test serialize Stats 
  *
 **/
 TEST(StatsSerializationTest_1, Success)
@@ -104,10 +105,10 @@ TEST(StatsSerializationTest_1, Success)
 
 #define TEST_S_INS(x, y) ASSERT_EQ((Stats::instance()->x == y), true)
 
-	TEST_S_INS(m_application, 		"stmodel");
-	TEST_S_INS(m_outFile,     		"file1");
-	TEST_S_INS(m_time,        		"time1");
-	TEST_S_INS(m_dataName,    		"dataName1");
+        TEST_S_INS(m_application, 		"stmodel");
+        TEST_S_INS(m_outFile,     		"file1");
+        TEST_S_INS(m_time,        		"time1");
+        TEST_S_INS(m_dataName,    		"dataName1");
         TEST_S_INS(m_range,                     t_range);
         TEST_S_INS(m_gProperty.m_numCC,         1234);
         TEST_S_INS(m_gProperty.m_numV,          t_numV);
@@ -124,7 +125,7 @@ TEST(StatsSerializationTest_1, Success)
 }
 
 /**
- * @brief   Test a small graph one color
+ * @brief   Test serialize st model 
  *
 **/
 TEST(CrimeSTModelSerializationTest_1, Success)
@@ -139,18 +140,39 @@ TEST(CrimeSTModelSerializationTest_1, Success)
     }
 
     {
-        CrimeSTModel stm1();
+        CrimeSTModel stm1;
         ifstream ifs("./filename");
         text_iarchive ia(ifs);
         ia>>stm1;
-        ASSERT_EQ
+        ASSERT_EQ((stm1 == stm), true);
     }
+
+    std::remove("./filename");
 }
 
 /**
- * @brief   Test a small graph one color
+ * @brief   Test serialize edges 
  *
 **/
 TEST(EdgeListSerializationTest_1, Success)
 {
+    string input_file = "../data/truss/ny_crime.csv"; 
+    CrimeSTModel stm(input_file);
+    edge_list edges = stm.build_edges(200, 200, 30);
+
+    {
+        ofstream ofs("./filename");
+        text_oarchive oa(ofs);
+        oa<<edges;
+    }
+
+    {
+        edge_list edges1;
+        ifstream ifs("./filename");
+        text_iarchive ia(ifs);
+        ia>>edges1;
+        ASSERT_EQ((edges == edges1), true);
+    }
+
+    std::remove("./filename");
 }
