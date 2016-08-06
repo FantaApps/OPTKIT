9ff1cd3f9dca24320b9271c9f38a0a2e498e5ff3
Cloud User <ec2-user@ip-172-31-5-200.us-west-2.compute.internal>
2016-08-06 14:48:09 -0400

    tempo

--- scripts/experiment_stmodel.sh
+++ scripts/experiment_stmodel.sh
@@ -19,69 +19,55 @@ DATA_HOME=${SCRIPT_HOME}/../data/
 mkdir -p ${DATA_HOME}/experiments/stmodel/
 
 #
-# For small NY crime test data
-#
-for XY in 100 200 
-do
-    for TIME in 0 30
-    do
-        echo process ${DATA_HOME}truss/ny_crime.csv with x $XY y $XY time $TIME
-        ${OPTKIT_HOME}optkit --stmodel                               \
-                             -i ${DATA_HOME}truss/ny_crime.csv  \
-                             -o ${DATA_HOME}experiments/stmodel/ny_crime_${XY}_${XY}_${TIME} \
-                             -c ${XY} -c ${XY} -c ${TIME} \
-                             -l ny_crime_${XY}_${XY}_${TIME}_
-        python ${SCRIPT_HOME}/OPTKITLoadData.py ${DATA_HOME}experiments/stmodel/ny_crime_${XY}_${XY}_${TIME}.json
-    done
-done
-
-#
 # For real CHI crime test data
 #
 for XY in 100 200 
 do
     for TIME in 0 30
     do
-        echo process ${DATA_HOME}stmodel/CHI_processed.csv with x $XY y $XY time $TIME
-        ${OPTKIT_HOME}optkit --stmodel                               \
-                             -i ${DATA_HOME}stmodel/CHI_processed.csv  \
-                             -o ${DATA_HOME}experiments/stmodel/CHI_${XY}_${XY}_${TIME} \
-                             -c ${XY} -c ${XY} -c ${TIME} \
-                             -l CHI_${XY}_${XY}_${TIME}_
-        python ${SCRIPT_HOME}/OPTKITLoadData.py ${DATA_HOME}experiments/stmodel/CHI_${XY}_${XY}_${TIME}.json
+        for FILE in "$(ls  ${DATA_HOME}stmodel/CHI/)"; 
+        do  
+            echo process ${DATA_HOME}stmodel/CHI/$FILE with x $XY y $XY time $TIME
+            #${OPTKIT_HOME}optkit --stmodel                               \
+            #                     -i ${DATA_HOME}stmodel/CHI/${FILE}  \
+            #                     -o ${DATA_HOME}experiments/stmodel/CHI/CHI_${FILE}_${XY}_${XY}_${TIME} \
+            #                     -c ${XY} -c ${XY} -c ${TIME} \
+            #                     -l CHI_${XY}_${XY}_${TIME}_
+            #python ${SCRIPT_HOME}/OPTKITLoadData.py ${DATA_HOME}experiments/stmodel/CHI/CHI_${FILE}_${XY}_${XY}_${TIME}.json
+        done
     done
 done
 
 #
 # For real DC crime test data
 #
-for XY in 100 200 
-do
-    for TIME in 0 30
-    do
-        echo process ${DATA_HOME}stmodel/DC_processed.csv with x $XY y $XY time $TIME
-        ${OPTKIT_HOME}optkit --stmodel                               \
-                             -i ${DATA_HOME}stmodel/DC_processed.csv  \
-                             -o ${DATA_HOME}experiments/stmodel/DC_${XY}_${XY}_${TIME} \
-                             -c ${XY} -c ${XY} -c ${TIME} \
-                             -l DC_${XY}_${XY}_${TIME}_
-        python ${SCRIPT_HOME}/OPTKITLoadData.py ${DATA_HOME}experiments/stmodel/DC_${XY}_${XY}_${TIME}.json
-    done
-done
+#for XY in 100 200 
+#do
+#    for TIME in 0 30
+#    do
+#        echo process ${DATA_HOME}stmodel/DC_processed.csv with x $XY y $XY time $TIME
+#        ${OPTKIT_HOME}optkit --stmodel                               \
+#                             -i ${DATA_HOME}stmodel/DC_processed.csv  \
+#                             -o ${DATA_HOME}experiments/stmodel/DC_${XY}_${XY}_${TIME} \
+#                             -c ${XY} -c ${XY} -c ${TIME} \
+#                             -l DC_${XY}_${XY}_${TIME}_
+#        python ${SCRIPT_HOME}/OPTKITLoadData.py ${DATA_HOME}experiments/stmodel/DC_${XY}_${XY}_${TIME}.json
+#    done
+#done
 
 #
 # For real NYC crime test data
 #
-for XY in 100 200 
-do
-    for TIME in 0 30
-    do
-        echo process ${DATA_HOME}stmodel/NY_processed.csv with x $XY y $XY time $TIME
-        ${OPTKIT_HOME}optkit --stmodel                               \
-                             -i ${DATA_HOME}stmodel/NY_processed.csv  \
-                             -o ${DATA_HOME}experiments/stmodel/NY_${XY}_${XY}_${TIME} \
-                             -c ${XY} -c ${XY} -c ${TIME} \
-                             -l NY_${XY}_${XY}_${TIME}_
-        python ${SCRIPT_HOME}/OPTKITLoadData.py ${DATA_HOME}experiments/stmodel/NY_${XY}_${XY}_${TIME}.json
-    done
-done
+#for XY in 100 200 
+#do
+#    for TIME in 0 30
+#    do
+#        echo process ${DATA_HOME}stmodel/NY_processed.csv with x $XY y $XY time $TIME
+#        ${OPTKIT_HOME}optkit --stmodel                               \
+#                             -i ${DATA_HOME}stmodel/NY_processed.csv  \
+#                             -o ${DATA_HOME}experiments/stmodel/NY_${XY}_${XY}_${TIME} \
+#                             -c ${XY} -c ${XY} -c ${TIME} \
+#                             -l NY_${XY}_${XY}_${TIME}_
+#        python ${SCRIPT_HOME}/OPTKITLoadData.py ${DATA_HOME}experiments/stmodel/NY_${XY}_${XY}_${TIME}.json
+#    done
+#done
