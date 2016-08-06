ad0f55e590e5d4a1e4cb8ca086114d43328c3eff
Cloud User <ec2-user@ip-172-31-5-200.us-west-2.compute.internal>
2016-08-06 17:58:42 -0400

    can run experiment in parallel

--- scripts/experiment_stmodel.sh
+++ scripts/experiment_stmodel.sh
@@ -6,6 +6,7 @@
 # @brief    This script runs experiment for stmodel 
 #
 #   MODIFIED   (MM/DD/YY)
+#   stplaydog   08/06/16 - Parallel computing 
 #   stplaydog   04/27/16 - Add upload result script 
 #   stplaydog   04/24/16 - Add logger support 
 #   stplaydog   04/16/16 - Add real data support 
@@ -18,56 +19,57 @@ DATA_HOME=${SCRIPT_HOME}/../data/
 
 mkdir -p ${DATA_HOME}/experiments/stmodel/
 
+rm ${SCRIPT_HOME}/process
+
+func_run_experiment()
+{
+    echo START >> ${SCRIPT_HOME}/process
+
+    local WHICH_FILE=$1
+    local FILE=$2
+    local XY=$3
+    local TIME=$4
+
+    echo process ${DATA_HOME}stmodel/${WHICH_FILE}/$FILE with x $XY y $XY time $TIME 
+
+    ${OPTKIT_HOME}optkit --stmodel                               \
+                         -i ${DATA_HOME}stmodel/${WHICH_FILE}/${FILE}  \
+                         -o ${DATA_HOME}experiments/stmodel/${WHICH_FILE}/${WHICH_FILE}_${FILE}_${XY}_${XY}_${TIME} \
+                         -c ${XY} -c ${XY} -c ${TIME} \
+                         -l ${WHICH_FILE}_${XY}_${XY}_${TIME}_
+
+    cp ${DATA_HOME}experiments/stmodel/${WHICH_FILE}/${WHICH_FILE}_${FILE}_${XY}_${XY}_${TIME}.json ${DATA_HOME}experiments/results/
+
+    echo END >> ${SCRIPT_HOME}/process
+}
+
 #
-# For real CHI crime test data
+# For real crime test data
 #
-for XY in 100 200 
+for WHICH_FILE in CHI DC NY
 do
-    for TIME in 0 30
+    for XY in 100 200 
     do
-        for FILE in "$(ls  ${DATA_HOME}stmodel/CHI/)"; 
-        do  
-            echo process ${DATA_HOME}stmodel/CHI/$FILE with x $XY y $XY time $TIME
-            #${OPTKIT_HOME}optkit --stmodel                               \
-            #                     -i ${DATA_HOME}stmodel/CHI/${FILE}  \
-            #                     -o ${DATA_HOME}experiments/stmodel/CHI/CHI_${FILE}_${XY}_${XY}_${TIME} \
-            #                     -c ${XY} -c ${XY} -c ${TIME} \
-            #                     -l CHI_${XY}_${XY}_${TIME}_
-            #python ${SCRIPT_HOME}/OPTKITLoadData.py ${DATA_HOME}experiments/stmodel/CHI/CHI_${FILE}_${XY}_${XY}_${TIME}.json
+        for TIME in 0 30
+        do
+            for FILE in $(ls  ${DATA_HOME}stmodel/${WHICH_FILE}/); 
+            do  
+
+                func_run_experiment ${WHICH_FILE} ${FILE} ${XY} ${TIME} & 
+
+                while true;
+                do
+                    NUM_START=$((`cat ${SCRIPT_HOME}/process | grep START | wc -l`))
+                    NUM_END=$((`cat ${SCRIPT_HOME}/process | grep END | wc -l`))
+                    counter=$((NUM_START- NUM_END))
+                    if [[ "$counter" -lt "16" ]]
+                    then
+                        break
+                    fi
+                    sleep 0.1
+                done
+
+            done
         done
     done
 done
-
-#
-# For real DC crime test data
-#
-#for XY in 100 200 
-#do
-#    for TIME in 0 30
-#    do
-#        echo process ${DATA_HOME}stmodel/DC_processed.csv with x $XY y $XY time $TIME
-#        ${OPTKIT_HOME}optkit --stmodel                               \
-#                             -i ${DATA_HOME}stmodel/DC_processed.csv  \
-#                             -o ${DATA_HOME}experiments/stmodel/DC_${XY}_${XY}_${TIME} \
-#                             -c ${XY} -c ${XY} -c ${TIME} \
-#                             -l DC_${XY}_${XY}_${TIME}_
-#        python ${SCRIPT_HOME}/OPTKITLoadData.py ${DATA_HOME}experiments/stmodel/DC_${XY}_${XY}_${TIME}.json
-#    done
-#done
-
-#
-# For real NYC crime test data
-#
-#for XY in 100 200 
-#do
-#    for TIME in 0 30
-#    do
-#        echo process ${DATA_HOME}stmodel/NY_processed.csv with x $XY y $XY time $TIME
-#        ${OPTKIT_HOME}optkit --stmodel                               \
-#                             -i ${DATA_HOME}stmodel/NY_processed.csv  \
-#                             -o ${DATA_HOME}experiments/stmodel/NY_${XY}_${XY}_${TIME} \
-#                             -c ${XY} -c ${XY} -c ${TIME} \
-#                             -l NY_${XY}_${XY}_${TIME}_
-#        python ${SCRIPT_HOME}/OPTKITLoadData.py ${DATA_HOME}experiments/stmodel/NY_${XY}_${XY}_${TIME}.json
-#    done
-#done
