fffefcb041b96a095e307a776090fcb3e08e3725
Cloud User <ec2-user@ip-172-31-5-200.us-west-2.compute.internal>
2016-08-13 22:55:30 -0400

    better data analysis

--- scripts/analysis_data.sh
+++ scripts/analysis_data.sh
@@ -3,7 +3,8 @@
 #
 # Copy right YMSys, 2015,2016 Zhaoming Yin
 #
-# @brief    This script runs experiment for stmodel 
+# @brief    This script runs data analysis to generate latex tables and 
+#           R figures.
 #
 #   MODIFIED   (MM/DD/YY)
 #   stplaydog   08/13/16 - Creation 
@@ -14,9 +15,10 @@ OPTKIT_HOME=${SCRIPT_HOME}/../src/
 DATA_HOME=${SCRIPT_HOME}/../data/
 ANALYSIS_HOME=${DATA_HOME}/analysis/
 RESULT_HOME=${DATA_HOME}/experiments/results/
+LOG_HOME=${SCRIPT_HOME}/../log/
 
 FILE_NAME=${ANALYSIS_HOME}result_`date +"%T"`
-python log_analysis.py -d ${RESULT_HOME} > ${ANALYSIS_HOME}tmp
+python log_analysis.py -d ${LOG_HOME} > ${ANALYSIS_HOME}tmp
 for COLUMN_ID in 2 3 4 5 6 7 
 do
     sort -t',' -n -k ${COLUMN_ID} ${ANALYSIS_HOME}tmp   > ${FILE_NAME}_${COLUMN_ID}
--- scripts/process_data_pattern.py
+++ scripts/process_data_pattern.py
@@ -12,6 +12,7 @@
 
 import sys
 from datetime import datetime
+from subprocess import call
 
 reader = open(sys.argv[1])
 
@@ -25,7 +26,7 @@ for line in lines:
         key =  items[4].strip().replace("/","").replace("  "," ").replace(" ", "_").replace("?", "")
         x = items[1].split(".")[0]
         y = items[2].split(".")[0]
-        if len(str(x)) >= 6 and len(str(x)) < 9 and len(str(y)) >= 6 and len(str(y)) < 9:
+        if len(str(x)) >= 6 and len(str(x)) < 9 and len(str(y)) >= 6 and len(str(y)) < 9 and int(x) >0 and int(y)>0:
             if key not in list:
                 list[key] = []
             list[key].append(items[0]+","+x+","+y+","+items[3]+","+key+"\n")
@@ -33,8 +34,36 @@ for line in lines:
         first_line = line
 
 for key in list:
-    writer = open(sys.argv[2]+"/"+key, "w")
+    file = sys.argv[2]+"/"+key
+    writer = open(file, "w")
     writer.write(first_line)
     for line in list[key]:
         writer.write(line)
     writer.close()
+
+    call(["sort", "-t,", "-n", "-k", "2", file, "-o", file+".sort"])
+
+    reader = open(file+".sort")
+    writer = open(file, "w")
+    dup_count = 0
+
+    pred       = []
+    pred_line  = []
+    pred_count = 0 
+    for line in reader.readlines():
+        cur = line.split(",")
+        if len(pred) != 0:
+            if pred[0] != cur[0] or pred[1] != cur[1] or pred[2] != cur[2]:
+                if pred_count >= 2:
+                    writer.write(pred_line.replace(",1", ","+str(pred_count)))
+                else:
+                    writer.write(pred_line)
+                pred_count = 1 
+            else:
+                dup_count  += 1
+                pred_count += 1
+        pred      = cur
+        pred_line = line
+    writer.close()
+    print file, "has ", str(dup_count), " duplications"
+    call(["rm", file+".sort"])
