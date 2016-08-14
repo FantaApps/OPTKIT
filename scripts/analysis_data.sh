285c521ef5aa253a790f98cad32800e7d322447b
Cloud User <ec2-user@ip-172-31-5-200.us-west-2.compute.internal>
2016-08-13 22:58:13 -0400

    data analysis on the result folder

--- scripts/analysis_data.sh
+++ scripts/analysis_data.sh
@@ -15,10 +15,9 @@ OPTKIT_HOME=${SCRIPT_HOME}/../src/
 DATA_HOME=${SCRIPT_HOME}/../data/
 ANALYSIS_HOME=${DATA_HOME}/analysis/
 RESULT_HOME=${DATA_HOME}/experiments/results/
-LOG_HOME=${SCRIPT_HOME}/../log/
 
 FILE_NAME=${ANALYSIS_HOME}result_`date +"%T"`
-python log_analysis.py -d ${LOG_HOME} > ${ANALYSIS_HOME}tmp
+python log_analysis.py -d ${RESULT_HOME} > ${ANALYSIS_HOME}tmp
 for COLUMN_ID in 2 3 4 5 6 7 
 do
     sort -t',' -n -k ${COLUMN_ID} ${ANALYSIS_HOME}tmp   > ${FILE_NAME}_${COLUMN_ID}
