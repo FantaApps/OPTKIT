bdf7b0e5e37cacb917079e34912043df77c9102d
Ubuntu <ubuntu@ip-172-31-7-235.us-west-2.compute.internal>
2016-09-19 06:32:24 +0000

    sth

--- scripts/data_analysis.R
+++ scripts/data_analysis.R
@@ -5,7 +5,7 @@ library(ggplot2)
 df<- read.csv(file=args[1],head=TRUE,sep=",")
 ggplot() +
 geom_line(data=df, aes(x = x, y = y, colour = data) ) +
-theme(legend.position="bottom", legend.text=element_text(size=12))
+theme(legend.position="bottom", legend.text=element_text(size=15))
 
 ggsave(file=args[2])
 
