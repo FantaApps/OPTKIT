27825c1f5d5e17d0ab1d7726a2ff811320d41b3f
Ubuntu <ubuntu@ip-172-31-7-235.us-west-2.compute.internal>
2016-10-12 01:12:32 +0000

    more fix

--- scripts/batch_prepare_mrdata.sh
+++ scripts/batch_prepare_mrdata.sh
@@ -6,6 +6,7 @@
 # @brief    This script runs experiment for stmodel 
 #
 #   MODIFIED   (MM/DD/YY)
+#   stplaydog   10/12/16 - Remove redundant logic 
 #   stplaydog   10/08/16 - Prepare configuration data
 #   stplaydog   10/07/16 - Support for CHI and NY 
 #   stplaydog   10/06/16 - Creation
@@ -13,39 +14,11 @@
 python prepare_mrdata.py ../data/stmodel/DC/BURGLARY      ../data/stmodel/DC/BUR/ 128 
 python prepare_mrdata.py ../data/stmodel/DC/ROBBERY       ../data/stmodel/DC/ROB/ 128 
 python prepare_mrdata.py ../data/stmodel/DC/LARCENY_THEFT ../data/stmodel/DC/TFT/ 128 
-aws s3 sync ../data/stmodel/DC/BUR/ s3://optkit11/DC_BUR/
-aws s3 sync ../data/stmodel/DC/ROB/ s3://optkit11/DC_ROB/
-aws s3 sync ../data/stmodel/DC/TFT/ s3://optkit11/DC_TFT/
-rm ../data/stmodel/DC/BUR/*
-rm ../data/stmodel/DC/ROB/*
-rm ../data/stmodel/DC/TFT/*
-python process_data_conf.py ../data/stmodel/DC/BURGLARY      ../data/experiment/mapreduce/DC_BUR_CONF 128
-python process_data_conf.py ../data/stmodel/DC/ROBBERY       ../data/experiment/mapreduce/DC_ROB_CONF 128
-python process_data_conf.py ../data/stmodel/DC/LARCENY_THEFT ../data/experiment/mapreduce/DC_TFT_CONF 128
 
 python prepare_mrdata.py ../data/stmodel/CHI/BURGLARY ../data/stmodel/CHI/BUR/ 128 
 python prepare_mrdata.py ../data/stmodel/CHI/ROBBERY  ../data/stmodel/CHI/ROB/ 128 
 python prepare_mrdata.py ../data/stmodel/CHI/THEFT    ../data/stmodel/CHI/TFT/ 128 
-aws s3 sync ../data/stmodel/CHI/BUR/ s3://optkit11/CHI_BUR/
-aws s3 sync ../data/stmodel/CHI/ROB/ s3://optkit11/CHI_ROB/
-aws s3 sync ../data/stmodel/CHI/TFT/ s3://optkit11/CHI_TFT/
-rm ../data/stmodel/CHI/BUR/*
-rm ../data/stmodel/CHI/ROB/*
-rm ../data/stmodel/CHI/TFT/*
-python process_data_conf.py ../data/stmodel/CHI/BURGLARY      ../data/experiment/mapreduce/CHI_BUR_CONF 128
-python process_data_conf.py ../data/stmodel/CHI/ROBBERY       ../data/experiment/mapreduce/CHI_ROB_CONF 128
-python process_data_conf.py ../data/stmodel/CHI/THEFT         ../data/experiment/mapreduce/CHI_TFT_CONF 128
 
 python prepare_mrdata.py ../data/stmodel/NY/BURGLARY      ../data/stmodel/NY/BUR/ 128 
 python prepare_mrdata.py ../data/stmodel/NY/ROBBERY       ../data/stmodel/NY/ROB/ 128 
 python prepare_mrdata.py ../data/stmodel/NY/GRAND_LARCENY ../data/stmodel/NY/TFT/ 128 
-aws s3 sync ../data/stmodel/NY/BUR/ s3://optkit11/NY_BUR/
-aws s3 sync ../data/stmodel/NY/ROB/ s3://optkit11/NY_ROB/
-aws s3 sync ../data/stmodel/NY/TFT/ s3://optkit11/NY_TFT/
-rm ../data/stmodel/NY/BUR/*
-rm ../data/stmodel/NY/ROB/*
-rm ../data/stmodel/NY/TFT/*
-python process_data_conf.py ../data/stmodel/NY/BURGLARY      ../data/experiment/mapreduce/NY_BUR_CONF 128
-python process_data_conf.py ../data/stmodel/NY/ROBBERY       ../data/experiment/mapreduce/NY_ROB_CONF 128
-python process_data_conf.py ../data/stmodel/NY/GRAND_LARCENY ../data/experiment/mapreduce/NY_TFT_CONF 128
-
--- scripts/emr_knox.sh
+++ scripts/emr_knox.sh
@@ -12,6 +12,7 @@
 SCRIPT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATA_HOME=${SCRIPT_HOME}/../data/
 CONFIG=$1
+SEQ=$1
 
 # upload according files to s3
 aws s3 cp ${SCRIPT_HOME}/mapper.py  s3://optkit11/
@@ -21,13 +22,7 @@ aws s3 cp ${DATA_HOME}/experiment/mapreduce/${CONFIG}_CONF s3://optkit11/EMR_CON
 # create a instance is there is none
 CLUSTER_ID=`aws emr list-clusters --active | grep Id | grep -o j-.* | sed -e s/\",//`
 
-# download results
-#mkdir -p ${DATA_HOME}experiment/results/EMR/${CONFIG}/
-#aws s3 sync s3://optkit11/results/${CONFIG} ${DATA_HOME}experiment/results/EMR/${CONFIG}/
-#aws s3 mv s3://optkit11/results/${CONFIG} s3://optkit11/results/${CONFIG}_ANOHTER
-#aws s3 rm s3://optkit11/results/${CONFIG} --recursive
-
 # run jobs
-cat emr_knox_temp.json | sed -e s/CONFIG/${CONFIG}/g | sed -e s/CONFIG/${CONFIG}/g >  emr_knox.json
+cat emr_knox_temp.json | sed -e s/CONFIG/${CONFIG}/g | sed -e s/SEQ/${SEQ}/g >  emr_knox.json
 aws emr add-steps --cluster-id ${CLUSTER_ID} --steps file://./emr_knox.json 
 
